d663a4710c5c185066767d7e689534646dc1760e
Jeremy Raymond <jeraymond@gmail.com>
2012-10-31 14:41:01 -0400

    Initial Commit

---
+++ NP Markdown/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NP Markdown
+//
+//  Created by Jeremy Raymond on 2012-10-31.
+//  Copyright (c) 2012 Jeremy Raymond. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
---
+++ NP MarkdownTests/NP_MarkdownTests.h
@@ -0,0 +1,13 @@
+//
+//  NP_MarkdownTests.h
+//  NP MarkdownTests
+//
+//  Created by Jeremy Raymond on 2012-10-31.
+//  Copyright (c) 2012 Jeremy Raymond. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NP_MarkdownTests : SenTestCase
+
+@end
