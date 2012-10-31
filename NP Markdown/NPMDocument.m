0f820191537f141142dd53216a57c304160fabc3
Jeremy Raymond <jeraymond@gmail.com>
2012-10-31 17:13:16 -0400

    Remove unused method

--- NP Markdown/NPMDocument.m
+++ NP Markdown/NPMDocument.m
@@ -26,12 +26,6 @@
     [self addWindowController:windowController];
 }
 
-- (void)windowControllerDidLoadNib:(NSWindowController *)aController
-{
-    [super windowControllerDidLoadNib:aController];
-    // Add any code here that needs to be executed once the windowController has loaded the document's window.
-}
-
 + (BOOL)autosavesInPlace
 {
     return YES;
